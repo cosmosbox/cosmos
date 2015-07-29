ec4340bb69495d771911a78dbeed91c3370d6b00
Mrkelly <chepy.v@gmail.com>
2015-07-29 17:42:42 +0800

    Test Etcd

--- src/CosmosTest/TestEtcd.cs
+++ src/CosmosTest/TestEtcd.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using MsgPack.Serialization;
 using NLog;
+using etcetera;
 
 namespace Cosmos.Test
 {
@@ -13,25 +14,67 @@ namespace Cosmos.Test
     public class TestEtcd
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private EtcdClient etcd;
+
+        [SetUp]
+        public void Init()
+        {
+            etcd = new EtcdClient(new Uri("http://localhost:4001/v2/keys/"));
+            
+        }
+
+        //[TearDown]
+        //void UnInit()
+        //{
+        //    etcd = null;
+        //}
+
         /// <summary>
         /// 务必Etcd在执行中
         /// </summary>
         /// <returns></returns>
         bool CheckEtcdRunning()
         {
-            return false;
+            try
+            {
+                var leader = etcd.Statistics.Leader();
+                return leader != null;
+            }
+            catch (Exception)
+            {
+                Logger.Warn("Etcd is not running !! Pass the test!!");
+                return false;
+            }
         }
 
-        [Test()]
-        public void TestEtcdAdd()
+        [Test]
+        public void TestEtcdSimpleSetAndAdd()
         {
-            Logger.Info("Check Etcd status...");
-            int a = 1;
-            int b = 2;
-            int sum = a + b;
-            Assert.AreEqual(sum, 3);
+            if (!CheckEtcdRunning())
+            {
+                Assert.Ignore();
+                return;
+            }
+            var respSet = etcd.Set("csharp-etcetera-test", "123");
+            Assert.AreEqual(respSet.Node.Value, "123");
+            var respGet = etcd.Get("csharp-etcetera-test");
+            Assert.AreEqual(respGet.Node.Value, "123");
         }
+        [Test]
 
+        public void TestEtcdDirSetAndAdd()
+        {
+            if (!CheckEtcdRunning())
+            {
+                Assert.Ignore();
+                return;
+            }
+            var respSet = etcd.Set("csharp-etcetera-test", "123");
+            Assert.AreEqual(respSet.Node.Value, "123");
+            var respGet = etcd.Get("csharp-etcetera-test");
+            Assert.AreEqual(respGet.Node.Value, "123");
+        }
     }
 }
 
