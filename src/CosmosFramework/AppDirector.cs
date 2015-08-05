faa7832b2e74f3d0f165ae3e80193ec2e6f9f521
Mrkelly <chepy.v@gmail.com>
2015-08-05 08:37:28 +0800

    cfg

--- src/CosmosFramework/AppDirector.cs
+++ src/CosmosFramework/AppDirector.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cosmos.Actor;
 
 namespace Cosmos.Framework
 {
     public abstract class AppDirector
     {
-
+		/// <summary>
+		/// Create all actor on actors.json
+		/// </summary>
         public virtual void StartAll()
         {
             
@@ -18,5 +21,25 @@ namespace Cosmos.Framework
         {
             
         }
+
+		/// <summary>
+		/// TODO: create a actor in thread!  not in config
+		/// </summary>
+		/// <returns>The thread actor.</returns>
+		/// <param name="config">Config.</param>
+		public virtual ActorNodeConfig NewThreadActor(ActorNodeConfig config)
+		{
+			return default(ActorNodeConfig);
+		}
+
+		/// <summary>
+		/// TODO: create a actor in new process!
+		/// </summary>
+		/// <returns>The process actor.</returns>
+		/// <param name="config">Config.</param>
+		public virtual ActorNodeConfig NewProcessActor(ActorNodeConfig config)
+		{
+			return default(ActorNodeConfig);
+		}
     }
 }
