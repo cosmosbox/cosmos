bf486a7da85d1b67841c8099190b7b12d5bd5ff6
Mrkelly <chepy.v@gmail.com>
2015-08-05 08:46:56 +0800

    proj conf loader

--- src/CosmosFramework/ProjectConfModule.cs
+++ src/CosmosFramework/ProjectConfModule.cs
@@ -1,21 +1,14 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Cosmos.Actor;
+using Newtonsoft.Json;
 
 namespace Cosmos.Framework
 {
-	public struct AppConfigInfo
-	{
-		public string AppName;
-		public string DirectorClass; // default: null, means "AppDirector"
-		public ActorNodeConfig[] Actors;
-	}
-
-
-
     /// <summary>
     /// A Module To Load confg
     /// </summary>
@@ -26,8 +19,21 @@ namespace Cosmos.Framework
         ProjectJsonConfLoader ConfLoader = new ProjectJsonConfLoader();
     }
 
+    public struct AppConfig
+    {
+        public string AppToken;
+    //    public ActorNodeConfig[] Actors { get; internal set; }
+    }
     public class ProjectJsonConfLoader
     {
-        
+        public AppConfig GetAppConfig(string configPath = "config/app.json")
+        {
+            if (!File.Exists(configPath))
+            {
+                throw new FileNotFoundException("Not found json discovery file", configPath);
+            }
+            var text = File.ReadAllText(configPath);
+            return JsonConvert.DeserializeObject<AppConfig>(text);
+        }
     }
 }
